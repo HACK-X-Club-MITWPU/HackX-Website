8e31f2906d907aa5df112699c202f8a07691c3be
Anupam Shekhar <ianupamshekhar@gmail.com>
2022-10-27 19:29:28 +0530

    add Social Buttons

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -25,18 +25,18 @@ const Footer = () => {
           <Column>
             <SocialButton
               URL="https://www.facebook.com/ianupamshekhar"
-              img="https://assets.leetcode.com/users/avatars/avatar_1655745431.png"
-              alt="Facebook"
-            />
-            <SocialButton
-              URL="https://www.instagram.com/ianupamshekhar/"
-              img="https://assets.leetcode.com/users/avatars/avatar_1655745431.png"
+              img="https://cdn-icons-png.flaticon.com/512/1384/1384015.png"
               alt="Instagram"
             />
             <SocialButton
-              URL="https://www.linkedin.com/in/anupamshekhar/"
-              img="https://assets.leetcode.com/users/avatars/avatar_1655745431.png"
+              URL="https://www.instagram.com/ianupamshekhar/"
+              img="https://cdn-icons-png.flaticon.com/512/1384/1384014.png"
               alt="LinkedIn"
+            />
+            <SocialButton
+              URL="https://www.linkedin.com/in/anupamshekhar/"
+              img="https://cdn-icons-png.flaticon.com/512/3293/3293282.png"
+              alt="Email"
             />
             <p>MITWPU, Pune</p>
           </Column>
