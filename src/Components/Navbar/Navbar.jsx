cd1704062a1598f04d80276c59a586d396e53e47
muhammmadarab <muhammadarab781@gmail.com>
2022-11-14 14:50:45 +0530

    navbar doneeeee

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,22 +1,34 @@
-import React from "react";
-import logo from "../../assets/img/logo.png"
-import './Navbar.css'
+import React, { useState } from "react";
+import { useLocation } from "react-router-dom";
+import logo from "../../assets/img/logo.png";
+import './Navbar.css';
+import { Link } from "react-router-dom";
+import MenuIcon from '@mui/icons-material/Menu';
+import CloseIcon from '@mui/icons-material/Close';
 
-export default function Navbar(){
-    return(
+export default function Navbar() {
+    const routePath = useLocation();
+    const [navShow, setNavShow] = useState(false);
+    const handleClick = () => setNavShow(!navShow);
+    return (
         <nav className="navbar">
-            <img src={logo}  />
-            <h3>HACK-X</h3>
-            <div className="nav-menu">
-                <ul>
-                    <li>Events</li>
-                    <li>|</li>
-                    <li>Team</li>
-                    <li>Events</li>
-                    <li>Achievments</li>
-                    <li>About us</li>
-                </ul>
+            <div className="navbar__logo">
+                <img src={logo} className="navbar__logo__img" alt="hack-x-logo-png" />
+                <h3 className="navbar__logo__h3">HACK-X</h3>
             </div>
+            <div className="toggle-button">
+                <button onClick={handleClick}>
+                    {navShow ? <CloseIcon /> : <MenuIcon />}
+                </button>
+            </div>
+            <ul className={`nav-menu ${!navShow && "hide"}`}>
+                <li className="nav-links">{routePath.pathname[1].toUpperCase() + routePath.pathname.slice(2)}</li>
+                <li>|</li>
+                <li className="nav-links"><Link to="/team">Team</Link></li>
+                <li className="nav-links"><Link to="/events">Events</Link></li>
+                <li className="nav-links"><Link to="/">Achievments</Link></li>
+                <li className="nav-links"><Link to="/about">About us</Link></li>
+            </ul>
         </nav>
     )
 }
